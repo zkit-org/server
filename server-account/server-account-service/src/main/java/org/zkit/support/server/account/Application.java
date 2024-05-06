48973e251af1ff1fc45c722a68e07234a66514c5
Grant <284885069@qq.com>
2024-05-06 21:04:18 +0800

    feat: 方法自调用

--- server-account/server-account-service/src/main/java/org/zkit/support/server/account/Application.java
+++ server-account/server-account-service/src/main/java/org/zkit/support/server/account/Application.java
@@ -3,11 +3,16 @@ package org.zkit.support.server.account;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.AdviceMode;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication(scanBasePackages = {"org.zkit.support"})
 @MapperScan("org.zkit.support.server.account.*.mapper")
 @EnableDiscoveryClient
+@EnableCaching(mode = AdviceMode.ASPECTJ)
+@EnableAspectJAutoProxy(exposeProxy=true)
 public class Application {
 
     public static void main(String[] args) {
