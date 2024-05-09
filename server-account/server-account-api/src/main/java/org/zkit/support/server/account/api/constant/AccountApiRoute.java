f29eda9832258db74a32523550d07f6b65d895f6
Grant <284885069@qq.com>
2024-05-09 17:31:19 +0800

    feat: 修改接口路径

--- server-account/server-account-api/src/main/java/org/zkit/support/server/account/api/constant/AccountApiRoute.java
+++ server-account/server-account-api/src/main/java/org/zkit/support/server/account/api/constant/AccountApiRoute.java
@@ -2,10 +2,10 @@ package org.zkit.support.server.account.api.constant;
 
 public class AccountApiRoute {
 
-    public static final String AUTH_FIND_BY_USERNAME = "/inner/auth/account/find/by/username";
-    public static final String AUTH_ACCOUNT_ADD = "/inner/auth/account/add";
-    public static final String AUTH_ACCOUNT_ADD_OR_GET = "/inner/auth/account/add/or/get";
-    public static final String AUTH_CREATE_TOKEN = "/inner/auth/create/token";
-    public static final String AUTH_ACCOUNT_OTP_SECRET = "/inner/auth/account/otp/secret";
+    public static final String AUTH_FIND_BY_USERNAME = "/api/internal/auth/account/find/by/username";
+    public static final String AUTH_ACCOUNT_ADD = "/api/internal/auth/account/add";
+    public static final String AUTH_ACCOUNT_ADD_OR_GET = "/api/internal/auth/account/add/or/get";
+    public static final String AUTH_CREATE_TOKEN = "/api/internal/auth/create/token";
+    public static final String AUTH_ACCOUNT_OTP_SECRET = "/api/internal/auth/account/otp/secret";
 
 }
