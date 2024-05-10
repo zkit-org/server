dace7e2bb613802615b018b928d8b17c509ae22f
Grant <284885069@qq.com>
2024-05-11 00:38:57 +0800

    feat: 规整项目

--- server-account/server-account-service/src/main/java/org/zkit/support/server/account/access/entity/mapstruct/AccessApiMapStruct.java
+++ server-account/server-account-service/src/main/java/org/zkit/support/server/account/access/entity/mapstruct/AccessApiMapStruct.java
@@ -2,8 +2,8 @@ package org.zkit.support.server.account.access.entity.mapstruct;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
-import org.zkit.support.cloud.starter.entity.SessionUser;
 import org.zkit.support.server.account.access.entity.dto.AccessApi;
+import org.zkit.support.starter.boot.entity.SessionUser;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface AccessApiMapStruct {
