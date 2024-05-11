995729de40b3caedd705c221f105b80b869d45f9
grant <284885069@qq.com>
2024-05-11 10:50:31 +0800

    feat: open api

--- server-account/server-account-api/src/main/java/org/zkit/support/server/account/api/entity/response/TokenResponse.java
+++ server-account/server-account-api/src/main/java/org/zkit/support/server/account/api/entity/response/TokenResponse.java
@@ -1,12 +1,18 @@
 package org.zkit.support.server.account.api.entity.response;
 
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
+@Schema(description = "令牌响应")
 public class TokenResponse {
 
+    @Schema(description = "令牌")
     private String token;
+    @Schema(description = "失效时间")
     private Long expiresIn;
+    @Schema(description = "用户名")
     private String username;
 
 }
--- server-account/server-account-service/src/main/java/org/zkit/support/server/account/inner/controller/InnerAccountController.java
+++ server-account/server-account-service/src/main/java/org/zkit/support/server/account/inner/controller/InnerAccountController.java
@@ -1,5 +1,8 @@
 package org.zkit.support.server.account.inner.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -16,40 +19,47 @@ import org.zkit.support.server.account.auth.service.AuthAccountService;
 
 @RestController
 @Slf4j
+@Tag(name = "[inner]account", description = "[内部接口]用户账户")
 public class InnerAccountController {
 
     private AuthAccountService authAccountService;
     private AuthAccountMapStruct authAccountMapStruct;
 
+    @Operation(summary = "根据用户名查找账户")
     @GetMapping(AccountApiRoute.AUTH_FIND_BY_USERNAME)
-    public AccountResponse findByUsername(@RequestParam("username") String username) {
+    public AccountResponse findByUsername(@Parameter(description = "用户名") @RequestParam("username") String username) {
         AuthAccount account = authAccountService.findByUsername(username);
         return authAccountMapStruct.toAccountResponse(account);
     }
 
     @PostMapping(AccountApiRoute.AUTH_ACCOUNT_ADD)
+    @Operation(summary = "添加账户")
     public AccountResponse add(@RequestBody() AccountAddRequest request) {
         AuthAccount account = authAccountService.add(authAccountMapStruct.toAuthAccount(request));
         return authAccountMapStruct.toAccountResponse(account);
     }
 
     @PostMapping(AccountApiRoute.AUTH_ACCOUNT_ADD_OR_GET)
+    @Operation(summary = "添加或获取账户")
     public AccountResponse addOrGet(@RequestBody() AccountAddRequest request) {
         AuthAccount account = authAccountService.addOrGet(authAccountMapStruct.toAuthAccount(request));
         return authAccountMapStruct.toAccountResponse(account);
     }
 
     @PostMapping(AccountApiRoute.AUTH_CREATE_TOKEN)
+    @Operation(summary = "创建Token")
     public TokenResponse createToken(@RequestBody() CreateTokenRequest request) {
         return authAccountService.createToken(request);
     }
 
     @PostMapping(AccountApiRoute.AUTH_ACCOUNT_OTP_SECRET)
+    @Operation(summary = "获取OTP密钥")
     public OTPResponse otpSecret(@RequestParam("id") Long id) {
         return authAccountService.otpSecret(id);
     }
 
     @PostMapping(AccountApiRoute.AUTH_ACCOUNT_SET_PASSWORD)
+    @Operation(summary = "设置密码")
     public TokenResponse setPassword(@RequestBody() SetPasswordRequest request) {
         return authAccountService.setPassword(request);
     }
