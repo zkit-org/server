737fdb464090085f2f0402a82a96924d3100f140
grant <284885069@qq.com>
2024-06-03 00:43:07 +0800

    feat: 翻译接口

---
+++ server-openai/server-openai-service/src/main/java/org/zkit/support/server/openai/entity/TranslatorRequest.java
@@ -0,0 +1,16 @@
+package org.zkit.support.server.openai.entity;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+@Data
+@Schema(description = "翻译请求")
+public class TranslatorRequest {
+
+    @Schema(description = "消息")
+    private String message;
+
+    @Schema(description = "目标")
+    private String target;
+
+}
