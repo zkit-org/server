94dd57e89dbfe0fa20607d30a287bcb786d017ad
Grant <284885069@qq.com>
2024-06-03 00:43:07 +0800

    feat: 翻译接口

--- server-openai/server-openai-service/src/main/java/org/zkit/support/server/openai/configurer/OpenAIConfigurer.java
+++ server-openai/server-openai-service/src/main/java/org/zkit/support/server/openai/configurer/OpenAIConfigurer.java
@@ -1,29 +1,21 @@
 package org.zkit.support.server.openai.configurer;
 
-import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
-import org.zkit.support.server.openai.configuration.OpenAIConfiguration;
+import org.springframework.context.annotation.Description;
+import org.zkit.support.server.openai.service.MockWeatherService;
+
+import java.util.function.Function;
 
 @Configuration
 @Slf4j
 public class OpenAIConfigurer {
 
-    @Resource
-    private OpenAIConfiguration configuration;
-
     @Bean
-    @Qualifier("openAIRestTemplate")
-    public RestTemplate openaiRestTemplate() {
-        RestTemplate restTemplate = new RestTemplate();
-        restTemplate.getInterceptors().add((request, body, execution) -> {
-            request.getHeaders().add("Authorization", "Bearer " + configuration.getToken());
-            return execution.execute(request, body);
-        });
-        return restTemplate;
+    @Description("Get the weather in location") // function description
+    public Function<MockWeatherService.Request, MockWeatherService.Response> weatherFunction() {
+        return new MockWeatherService();
     }
 
 }
---
+++ server-openai/server-openai-service/src/main/java/org/zkit/support/server/openai/controller/OpenAIController.java
@@ -0,0 +1,24 @@
+package org.zkit.support.server.openai.controller;
+
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import org.zkit.support.server.openai.entity.TranslatorRequest;
+import org.zkit.support.server.openai.service.TranslatorService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/openai")
+@Slf4j
+public class OpenAIController {
+
+    @Resource
+    private TranslatorService translatorService;
+
+    @PostMapping("/translator")
+    public List<String> translator(@RequestBody TranslatorRequest request) {
+        return translatorService.translate(request);
+    }
+
+}
