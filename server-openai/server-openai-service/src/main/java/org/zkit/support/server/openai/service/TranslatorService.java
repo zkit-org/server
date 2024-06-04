7ed56a1acb1801948de29d01cfffeebda848a9b1
Grant <284885069@qq.com>
2024-06-04 14:29:01 +0800

    feat: 增加缓存；增加配置项

--- server-openai/server-openai-service/src/main/java/org/zkit/support/server/openai/service/TranslatorService.java
+++ server-openai/server-openai-service/src/main/java/org/zkit/support/server/openai/service/TranslatorService.java
@@ -1,13 +1,19 @@
 package org.zkit.support.server.openai.service;
 
+import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.messages.Message;
 import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.zkit.support.server.openai.api.entity.TranslatorRequest;
+import org.zkit.support.server.openai.configuration.TranslatorConfiguration;
+import org.zkit.support.starter.boot.utils.MD5Utils;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
 
 @Service
@@ -15,19 +21,27 @@ import java.util.stream.Stream;
 public class TranslatorService {
 
     private final ChatClient chatClient;
+    @Resource
+    private RedisTemplate<String, String> redisTemplate;
+    @Resource
+    private TranslatorConfiguration configuration;
 
     TranslatorService(ChatClient.Builder chatClientBuilder) {
         this.chatClient = chatClientBuilder.build();
     }
 
     public List<String> translate(TranslatorRequest request){
-        List<Message> messages = List.of(
-                new SystemMessage("你是一个翻译助手。"),
-                new SystemMessage("请给出多条翻译结果，使用,分割，最多10条；最符合的排序靠前；词性尽可能保持一致；中文繁体简体取一条。"),
-                new SystemMessage("请翻译成 "+request.getTarget()+"，使用 "+request.getTarget()+" 回复结果，且只需要回复翻译结果。")
-        );
-        String result = chatClient.prompt().messages(messages).user(request.getMessage()).call().content();
-        log.info("origin: {}", result);
+        String messageHash = MD5Utils.text(request.getMessage());
+        String cacheKey = "openai:translator:" + request.getTarget() + ":" + messageHash;
+        String result = redisTemplate.opsForValue().get(cacheKey);
+        log.info("origin in cache: {}", result);
+        if(result == null) {
+            List<Message> messages = new ArrayList<>();
+            configuration.getPrompt().forEach(prompt -> messages.add(new SystemMessage(prompt.replaceAll("\\{target}", request.getTarget()))));
+            result = chatClient.prompt().messages(messages).user(request.getMessage()).call().content();
+            log.info("origin: {}", result);
+            redisTemplate.opsForValue().set(cacheKey, result, configuration.getExpiration(), TimeUnit.SECONDS);
+        }
         return Stream.of(result.split(",")).map(String::trim).distinct().toList();
     }
 }
