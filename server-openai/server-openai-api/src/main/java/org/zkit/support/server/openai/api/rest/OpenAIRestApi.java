5841f862df4dd8928b36c75d6f1c18987529a203
Grant <284885069@qq.com>
2024-06-05 11:31:16 +0800

    feat: use http service

--- server-openai/server-openai-api/src/main/java/org/zkit/support/server/openai/api/rest/OpenAIRestApi.java
+++ server-openai/server-openai-api/src/main/java/org/zkit/support/server/openai/api/rest/OpenAIRestApi.java
@@ -4,11 +4,11 @@ import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.TypeReference;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
-import okhttp3.*;
 import org.springframework.stereotype.Service;
 import org.zkit.support.server.openai.api.configuration.OpenAIConfiguration;
 import org.zkit.support.server.openai.api.entity.TranslatorRequest;
 import org.zkit.support.starter.boot.entity.Result;
+import org.zkit.support.starter.boot.okhttp.HTTPService;
 
 import java.util.List;
 
@@ -19,21 +19,15 @@ public class OpenAIRestApi {
     @Resource
     private OpenAIConfiguration configuration;
     @Resource
-    private OkHttpClient client;
+    private HTTPService httpService;
 
     public List<String> translator(TranslatorRequest data) {
-        RequestBody requestBody = RequestBody.create(JSON.toJSONString(data), MediaType.parse("application/json; charset=utf-8"));
-        Request request = new Request.Builder().url(configuration.getBaseUrl()+"/api/openai/translator").post(requestBody).build();
         try{
-            try (Response response = client.newCall(request).execute()) {
-                if (response.body() != null) {
-                    String body = response.body().string();
-                    Result<List<String>> result = JSON.parseObject(body, new TypeReference<Result<List<String>>>(){});
-                    log.info("result: {}", result);
-                    if (result != null && result.getData() != null) {
-                        return result.getData();
-                    }
-                }
+            String body = httpService.post(configuration.getBaseUrl()+"/api/openai/translator", JSON.toJSONString(data));
+            Result<List<String>> result = JSON.parseObject(body, new TypeReference<Result<List<String>>>(){});
+            log.info("result: {}", result);
+            if (result != null && result.getData() != null) {
+                return result.getData();
             }
         }catch (Exception e) {
             log.error(e.getMessage());
