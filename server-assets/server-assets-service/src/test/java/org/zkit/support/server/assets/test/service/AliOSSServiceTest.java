cf3ce2f02de3e8bbaed4cad44a91495de3339886
Grant <284885069@qq.com>
2024-07-05 19:34:07 +0800

    feat: 优化时间

--- server-assets/server-assets-service/src/test/java/org/zkit/support/server/assets/test/service/AliOSSServiceTest.java
+++ server-assets/server-assets-service/src/test/java/org/zkit/support/server/assets/test/service/AliOSSServiceTest.java
@@ -1,6 +1,7 @@
 package org.zkit.support.server.assets.test.service;
 
 import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.zkit.support.server.assets.service.AliOSSService;
@@ -10,6 +11,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @SpringBootTest
+@Slf4j
 public class AliOSSServiceTest {
 
     @Resource
@@ -17,9 +19,10 @@ public class AliOSSServiceTest {
 
     @Test
     public void testSign() throws IOException {
-        String signedUrl = aliOSSService.sign("test.txt");
+        String signedUrl = aliOSSService.sign("test.pdf");
+        log.info("signedUrl {}", signedUrl);
         // 填写本地文件的完整路径。如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件。
-        String pathName = "/Users/zhaozhenhua/Downloads/test.txt";
+        String pathName = "/Users/zhaozhenhua/Downloads/test.pdf";
 
         Map<String, String> headers = new HashMap<String, String>();
         Map<String, String> userMetadata = new HashMap<String, String>();
