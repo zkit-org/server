4a0c1bc073eca03fe451e1b0ce330f7e89a01888
grant <284885069@qq.com>
2024-07-20 22:50:32 +0800

    feat: 重构AI接口

--- server-ai/server-ai-api/src/main/java/org/zkit/support/server/ai/api/ServerAIApiLoader.java
+++ server-ai/server-ai-api/src/main/java/org/zkit/support/server/ai/api/ServerAIApiLoader.java
@@ -2,6 +2,6 @@ package org.zkit.support.server.ai.api;
 
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients(basePackageClasses = ServerOpenAIApiLoader.class)
-public class ServerOpenAIApiLoader {
+@EnableFeignClients(basePackageClasses = ServerAIApiLoader.class)
+public class ServerAIApiLoader {
 }
