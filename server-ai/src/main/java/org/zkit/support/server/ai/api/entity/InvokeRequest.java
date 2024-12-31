17902936ad6652a05da946e94c73570d6eb9a824
Grant <284885069@qq.com>
2025-01-01 01:22:13 +0800

    feat: 优化请求

--- server-ai/src/main/java/org/zkit/support/server/ai/api/entity/InvokeRequest.java
+++ server-ai/src/main/java/org/zkit/support/server/ai/api/entity/InvokeRequest.java
@@ -3,10 +3,13 @@ package org.zkit.support.server.ai.api.entity;
 import com.alibaba.fastjson2.JSONObject;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class InvokeRequest {
 
     private String content;
+    private List<String> messages;
     private JSONObject filter;
 
 }
--- server-ai/src/main/java/org/zkit/support/server/ai/api/service/AIAPIService.java
+++ server-ai/src/main/java/org/zkit/support/server/ai/api/service/AIAPIService.java
@@ -13,6 +13,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.zkit.support.server.ai.api.entity.InvokeRequest;
+import org.zkit.support.server.ai.api.entity.Message;
 import org.zkit.support.starter.boot.entity.Result;
 
 import java.util.List;
@@ -29,7 +30,14 @@ public class AIAPIService {
     public Result<String> invoke(InvokeRequest request){
         InvokeHttpEntity httpEntity = new InvokeHttpEntity();
         httpEntity.setContent(request.getContent());
-        httpEntity.setMessages(configuration.getPrompts());
+        List<Message> messages = configuration.getPrompts();
+        request.getMessages().forEach(m -> {
+            Message message = new Message();
+            message.setRole("user");
+            message.setContent(m);
+            messages.add(message);
+        });
+        httpEntity.setMessages(messages);
         httpEntity.setLimit(configuration.getLimit());
         httpEntity.setFilter(request.getFilter());
 
