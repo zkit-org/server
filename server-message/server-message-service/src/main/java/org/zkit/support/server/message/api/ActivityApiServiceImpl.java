e48b17a81049799e5e2b8e5420c61fca58a6a033
Grant <284885069@qq.com>
2025-07-15 00:34:00 +0800

    feat: clean

--- server-message/server-message-service/src/main/java/org/zkit/support/server/message/api/ActivityApiServiceImpl.java
+++ server-message/server-message-service/src/main/java/org/zkit/support/server/message/api/ActivityApiServiceImpl.java
@@ -3,7 +3,6 @@ package org.zkit.support.server.message.api;
 import org.apache.dubbo.config.annotation.DubboService;
 import org.zkit.support.server.message.activity.service.ActivityService;
 import org.zkit.support.server.message.api.entity.request.ActivityListRequest;
-import org.zkit.support.server.message.api.entity.request.ActivityRequest;
 import org.zkit.support.server.message.api.entity.response.ActivityResponse;
 import org.zkit.support.server.message.api.service.ActivityApiService;
 import org.zkit.support.starter.mybatis.entity.PageResult;
