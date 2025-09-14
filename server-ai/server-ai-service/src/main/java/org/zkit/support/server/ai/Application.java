12d6e243d0b171ed524f4e766dcb8033292dd973
Grant <284885069@qq.com>
2025-09-15 00:53:49 +0800

    feat: add FastjsonAutoTypePackages

--- server-ai/server-ai-service/src/main/java/org/zkit/support/server/ai/Application.java
+++ server-ai/server-ai-service/src/main/java/org/zkit/support/server/ai/Application.java
@@ -7,8 +7,10 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.AdviceMode;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.zkit.support.starter.boot.fastjson.FastjsonAutoTypePackages;
 
 @SpringBootApplication
+@FastjsonAutoTypePackages({ "org.zkit.support" })
 @EnableDiscoveryClient
 @EnableCaching(mode = AdviceMode.ASPECTJ)
 @EnableAspectJAutoProxy(exposeProxy=true)
--- server-message/server-message-service/src/main/java/org/zkit/support/server/message/Application.java
+++ server-message/server-message-service/src/main/java/org/zkit/support/server/message/Application.java
@@ -8,9 +8,11 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.AdviceMode;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.zkit.support.starter.boot.fastjson.FastjsonAutoTypePackages;
 
 @SpringBootApplication
 @MapperScan("org.zkit.support.server.message.*.mapper")
+@FastjsonAutoTypePackages({ "org.zkit.support" })
 @EnableDiscoveryClient
 @EnableCaching(mode = AdviceMode.ASPECTJ)
 @EnableAspectJAutoProxy(exposeProxy=true)
