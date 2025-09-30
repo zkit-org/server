0a242bf6865238996c7a1afce340ddccc0813f8e
Grant <284885069@qq.com>
2025-10-01 01:22:12 +0800

    feat: profile api

--- server-authub/src/main/java/org/zkit/support/server/authub/account/controller/AccountController.java
+++ server-authub/src/main/java/org/zkit/support/server/authub/account/controller/AccountController.java
@@ -1,7 +1,13 @@
 package org.zkit.support.server.authub.account.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.zkit.support.starter.security.annotation.CurrentUser;
+import org.zkit.support.starter.security.entity.SessionUser;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 
 /**
  * <p>
@@ -12,7 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2025-09-29
  */
 @RestController
-@RequestMapping("/account/account")
+@RequestMapping("/account")
 public class AccountController {
+
+    @GetMapping("/profile")
+    @Operation(summary = "当前用户信息")
+    public SessionUser profile(@CurrentUser() @Parameter(hidden = true) SessionUser user) {
+        return user;
+    }
 
 }
